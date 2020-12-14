72d9a1c3e583c411f209a7e05710a42240c1b5b0
Cheng Shao <cheng.shao38@gmail.com>
2020-12-14 16:45:47 -0500

    First commit.

---
+++ caesar-cipher.rb
@@ -0,0 +1,29 @@
+def caesar_cipher(phrase,shift) 
+    p phrase
+    arr = phrase.split("").map do |char| 
+        code = char.ord
+        if code.between?(65,90) 
+            code = wrap_char(code+shift, 65)
+        end
+            
+        if code.between?(97,122)
+            code = wrap_char(code+shift, 97)                
+        end
+        code.chr
+    end
+    arr.join
+
+end
+
+def wrap_char(code, lowest)
+    (code - lowest)%26 + lowest
+end
+
+p caesar_cipher("What a string!", 5)
+p caesar_cipher("hello",5)
+p caesar_cipher("Hello",5)
+p caesar_cipher("ZAizt",10)
+p caesar_cipher("hello",-5)
+p caesar_cipher("hello qweqwe",5)
+p caesar_cipher(".hello qweqwe!!!",5)
+
