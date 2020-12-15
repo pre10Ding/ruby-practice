54d45039ec55b81207de42e0cf6eca530591a950
Cheng Shao <cheng.shao38@gmail.com>
2020-12-14 21:15:16 -0500

    Reformat comments for clarity.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,18 +2,19 @@ def bubble_sort(nums)
     for j in 1..nums.length # j tracks iterations, 
         sorted_flag = true # break if nothing needs to be sorted
         nums.map!.with_index do |num, i|
-            if i<nums.length-j && num > nums[i+1] #the last item of every iter is sorted so no need to go that far
+            if i<nums.length-j && num > nums[i+1] # the last item of every iter is sorted correctly, 
+                                                  # so no need to go that far
                 num_next = nums[i+1]
                 nums[i+1] = num
                 sorted_flag = false
-                num_next #map the next num to curr num
+                num_next # map the next num to curr num
             else
-                num #map the curr num to curr num
+                num # map the curr num to curr num
             end
         end
         break if sorted_flag # break if nothing needs to be sorted
     end
-    nums #return value
+    nums # return value
 end
 
 
