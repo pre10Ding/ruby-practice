ba5c5223a3b071a990a3d91810086765d18bf872
Cheng Shao <cheng.shao38@gmail.com>
2021-01-15 01:42:58 -0500

    Wrap cipher method in a class.

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,29 +1,31 @@
-def caesar_cipher(phrase,shift) 
-    p phrase
-    arr = phrase.split("").map do |char| 
-        code = char.ord
-        if code.between?(65,90) 
-            code = wrap_char(code+shift, 65)
+class CaesarCipher
+    def initialize; end
+    def caesar_cipher(phrase,shift) 
+        p phrase
+        arr = phrase.split("").map do |char| 
+            code = char.ord
+            if code.between?(65,90) 
+                code = wrap_char(code+shift, 65)
+            end
+                
+            if code.between?(97,122)
+                code = wrap_char(code+shift, 97)                
+            end
+            code.chr
         end
-            
-        if code.between?(97,122)
-            code = wrap_char(code+shift, 97)                
-        end
-        code.chr
+        arr.join
+
     end
-    arr.join
 
+    def wrap_char(code, lowest)
+        (code - lowest)%26 + lowest
+    end
 end
-
-def wrap_char(code, lowest)
-    (code - lowest)%26 + lowest
-end
-
-p caesar_cipher("What a string!", 5)
-p caesar_cipher("hello",5)
-p caesar_cipher("Hello",5)
-p caesar_cipher("ZAizt",10)
-p caesar_cipher("hello",-5)
-p caesar_cipher("hello qweqwe",5)
-p caesar_cipher(".hello qweqwe!!!",5)
+# p caesar_cipher("What a string!", 5)
+# p caesar_cipher("hello",5)
+# p caesar_cipher("Hello",5)
+# p caesar_cipher("ZAizt",10)
+# p caesar_cipher("hello",-5)
+# p caesar_cipher("hello qweqwe",5)
+# p caesar_cipher(".hello qweqwe!!!",5)
 
